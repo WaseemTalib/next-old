f8c94409076be921f36c4c9a2e3c07a0a40b1cfa
WaseemTalib <waseemltalib@gmail.com>
2023-12-12 02:51:55 +0500

    Pages Added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 function Home() {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push('/products')
+  }
+
   return (
+    <div>
     <h1>Home Page</h1>
+    <h2><Link href={'/blog'}>Blog</Link></h2>
+    <h2><Link href={'/products'}>Products</Link></h2>
+    <button onClick={handleClick}>Place Order</button>
+    </div>
   )
 }
 
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
-
+import Link from 'next/link'
 function Products() {
   return (
     <div>
       <h1>Products List Page</h1>
+      <h2><Link href={'/'}>Home Page</Link></h2>
       <>
-        <h2>product 1</h2>
-        <h2>product 2</h2>
-        <h2>product 3</h2>
+        <h2>
+          <Link href={'products/1'} replace>Product 1</Link>
+        </h2>
+        <h2>
+          <Link href={'products/1'}>Product 2</Link>
+        </h2>
+        <h2>
+          <Link href={'products/1'}>Product 3</Link>
+        </h2>
       </>
     </div>
   )
